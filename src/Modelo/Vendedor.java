e0960c777c1bf5390809d57465495b8fe5f04321
sergiopdf-x <stoapanta887@gmail.com>
2026-04-14 01:53:41 +0200

    Aplicación de getter, setter con validaciones, métodos

---
+++ src/Modelo/Vendedor.java
@@ -0,0 +1,76 @@
+package Modelo;
+
+public class Vendedor {
+    public String nombre;
+    public String area;
+    private double montoVendido;
+    private double porcentajeComision;
+    private double cumplimientoMeta;
+
+    public Vendedor(String nombre, String area, double montoVendido, double porcentajeComision , double cumplimientoMeta){
+        this.nombre = nombre;
+        this.area = area;
+        this.montoVendido = montoVendido;
+        this.porcentajeComision = porcentajeComision;
+        this.cumplimientoMeta = cumplimientoMeta;
+    }
+
+    //Getters
+    public String getNombre(){
+        return  nombre;
+    }
+
+    public String getArea(){
+        return area;
+    }
+
+    public double getMontoVendido(){
+        return montoVendido;
+    }
+
+    public double getPorcentajeComision(){
+        return porcentajeComision;
+    }
+
+    public double getCumplimientoMeta(){
+        return cumplimientoMeta;
+    }
+
+    //Setter con validaciones
+    public void setMontoVendido(){
+        if(montoVendido > 0){
+            this.montoVendido = montoVendido;
+        }else{
+            System.out.println("El monto no es valido");
+        }
+    }
+
+
+    public double calcularComision(){
+        return montoVendido * (porcentajeComision/100);
+    }
+
+    public double calcularIngresoTotal(){
+        return  montoVendido + calcularComision();
+    }
+
+    public String obtenerEstadoCumplimiento(){
+        if(cumplimientoMeta >= 90){
+            return "Excelente";
+        }else if(cumplimientoMeta >=70){
+            return "Aceptable";
+        }else {
+            return "Bajo";
+        }
+    }
+
+    public String obtenerMensajeDesempeño(){
+        if (cumplimientoMeta >= 90){
+            return "Vendedor con empeño sobresaliente";
+        }else if (cumplimientoMeta >=70){
+            return "Vendedor con desempeño aceptable";
+        }else{
+            return "Se requiere seguimiento comercial";
+        }
+    }
+}
